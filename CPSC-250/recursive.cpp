3fc983abb2deb362795701a9b9d779b14c2d937d
John Winward <john.winward2@gmail.com>
2021-09-13 15:43:03 -0400

    finish HandsOn1 part 1

--- CPSC-250/recursive.cpp
+++ CPSC-250/recursive.cpp
@@ -38,21 +38,29 @@ int main()
 		<< endl; //in miliseconds
 
 	//recursive writeBackward function
-	cout << endl << "The output for the writeBackward function is ";
+	cout << endl << "The output for the recursive writeBackward function is ";
 	start = clock();
 	writeBackward(s);
 	stop = clock();
 	cout << endl << "Running time for the function is " << static_cast<double>(stop - start) << endl;
 
 	//iterative writeBackward function
-	cout << endl << "The output for the writeBackward function is ";
+	cout << endl << "The output for the iterative writeBackward function is ";
 	start = clock();
 	IterativewriteBackward(s);
 	stop = clock();
 	cout << endl << "Running time for the function is " << static_cast<double>(stop - start) << endl;
 
+	//recursive binary search function
+	cout << endl << "(Recursive) The index of the number in the array is ";
+	start = clock();
+	res = binarySearch(array, 0, SIZE2 - 1, 123123);
+	stop = clock();
+	cout << res;
+	cout << endl << "Running time for the function is " << static_cast<double>(stop - start) << endl;
+
 	//iterative binary search function
-	cout << endl << "The index of the number in the array is ";
+	cout << endl << "(Iterative) The index of the number in the array is ";
 	start = clock();
 	res = IterativeBinarySearch(array, SIZE2, 123123);
 	stop = clock();
@@ -111,6 +119,19 @@ void IterativewriteBackward(string s)
 		new_s += s.substr(i, 1);
 	}
 	cout << new_s;
+}
+
+int binarySearch(const int anArray[], int first, int last, int target)
+{
+	int i = (last - first) / 2 + first;
+	if (anArray[i] == target)
+		return i;
+	else if (last - first <= 1)
+		return -1;
+	else if (anArray[i] < target)
+		return binarySearch(anArray, i, last, target);
+	else if (anArray[i] > target)
+		return binarySearch(anArray, first, i, target);
 }
 
 int IterativeBinarySearch(int array[], int size, int target)
