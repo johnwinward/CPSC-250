4edc52d6c49617a24abacf3599bb82f52219a294
John Winward <john.winward2@gmail.com>
2021-10-04 14:34:07 -0400

    initialize HandsOn4

---
+++ CPSC-250/ComparisonCounter.cpp
@@ -0,0 +1,18 @@
+/*John Winward
+* CPSC 250
+* 10/4/2021
+* Hands-On Exercise 4 */
+#include <iostream>
+
+using namespace std;
+
+//sorting algorithms
+
+
+int main()
+{
+	
+
+	system("pause");
+	return 0;
+}
