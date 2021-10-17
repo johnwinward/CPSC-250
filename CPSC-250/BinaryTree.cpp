1b34c2024e3c65e401c4bda03f3d9974f6cd058a
John Winward <john.winward2@gmail.com>
2021-10-17 10:41:33 -0400

    finished HandsOn5

--- CPSC-250/BinaryTree.cpp
+++ CPSC-250/BinaryTree.cpp
@@ -1,5 +1,5 @@
 /*John Winward
-  10/13/2021
+  10/17/2021
   CPSC 250
   Hands-On Exercise 5 */
 #include <iostream>
@@ -34,10 +34,13 @@ int main()
 
 	//initialize three
 	three.info = 5;
+	three.left = NULL;
 	three.right = &six;
 	
 	//initialize four
 	four.info = 2;
+	four.left = NULL;
+	four.right = NULL;
 
 	//initialize five
 	five.info = 6;
@@ -47,16 +50,65 @@ int main()
 	//initialize six
 	six.info = 9;
 	six.left = &nine;
+	six.right = NULL;
 
 	//initialize seven
 	seven.info = 5;
+	seven.left = NULL;
+	seven.right = NULL;
 
 	//initialize eight
 	eight.info = 11;
+	eight.left = NULL;
+	eight.right = NULL;
 
 	//initialize nine
 	nine.info = 4;
+	nine.left = NULL;
+	nine.right = NULL;
+
+	//Test preOrder
+	cout << "Pre-Order Traversal: ";
+	preOrder(root);
+	cout << endl;
+
+	//Test inOrder
+	cout << "In-Order Traversal: ";
+	inOrder(root);
+	cout << endl;
+
+	//Test postOrder
+	cout << "Post-Order Traversal: ";
+	postOrder(root);
+	cout << endl;
 
 	system("pause");
 	return 0;
+}
+
+void preOrder(TreeNode* r)
+{
+	cout << r->info << " ";
+	if(r->left != NULL)
+		preOrder(r->left);
+	if(r->right != NULL)
+		preOrder(r->right);
+}
+
+void inOrder(TreeNode* r)
+{
+	if (r->left != NULL)
+		inOrder(r->left);
+	cout << r->info << " ";
+	if (r->right != NULL)
+		inOrder(r->right);
+}
+
+void postOrder(TreeNode* r)
+{
+	if (r->left != NULL)
+		postOrder(r->left);
+	if (r->right != NULL)
+		postOrder(r->right);
+	cout << r->info << " ";
 }
