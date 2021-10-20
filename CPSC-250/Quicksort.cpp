050145e96f2a0c91ea6aec705dbad0f52e53c19d
John Winward <john.winward2@gmail.com>
2021-10-20 14:42:46 -0400

    progress made in class 10/20

--- CPSC-250/Quicksort.cpp
+++ CPSC-250/Quicksort.cpp
@@ -5,3 +5,28 @@
 	10/18/2021
 	Programming Assignment 2: Algorithm Analysis (Quicksort)
 */
+#include <iostream>
+
+using namespace std;
+
+void quickSort(int a[], int first, int last);
+void partition(int a[], int first, int last);
+void quickSortMod(int a[], int first, int last);
+void partitionMod(int a[], int first, int last);
+void buildArray(int a[], int n);
+
+int main()
+{
+	//
+
+	system("pause");
+	return 0;
+}
+
+void buildArray(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = n - i;
+	}
+}
