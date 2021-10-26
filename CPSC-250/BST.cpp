b7d595227f0c0ae3af2ad49c4f8cd713b133772a
John Winward <john.winward2@gmail.com>
2021-10-26 09:05:52 -0400

    progress made

--- CPSC-250/BST.cpp
+++ CPSC-250/BST.cpp
@@ -23,7 +23,46 @@ TreeNode* TreeSearch(TreeNode*, int);
 int main()
 {
 	//create Binary Search Tree
+	TreeNode* r = NULL;
+	insert(r, 10);
+	insert(r, 18);
+	insert(r, 7);
+	insert(r, 2);
+	insert(r, 20);
+	insert(r, 15);
+
+	inOrder(r);
 
 	system("pause");
 	return 0;
+}
+
+void inOrder(TreeNode* node)
+{
+	if (node->left != NULL)
+		inOrder(node->left);
+	cout << node->info << " ";
+	if (node->right != NULL)
+		inOrder(node->right);
+}
+
+void insert(TreeNode*& node, int num)
+{
+	TreeNode newNode;
+	newNode.left = NULL;
+	newNode.right = NULL;
+	newNode.info = num;
+	while (node != NULL)
+		node = node->left;
+	node = &newNode;
+}
+
+int countNodes(TreeNode* node)
+{
+	int total = 0;
+	if (node->left != NULL)
+		total += countNodes(node->left);
+	if (node->right != NULL)
+		total += countNodes(node->right);
+	return total;
 }
