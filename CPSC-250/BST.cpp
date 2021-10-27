aacc78785042977318cbb01deac5444bcf2ee3c0
John Winward <john.winward2@gmail.com>
2021-10-26 23:45:07 -0400

    done

--- CPSC-250/BST.cpp
+++ CPSC-250/BST.cpp
@@ -31,12 +31,27 @@ int main()
 	insert(r, 20);
 	insert(r, 15);
 
+	//Test
 	inOrder(r);
+	cout << endl;
+
+	cout << "Count: " << countNodes(r) << endl;
+
+	if (TreeSearch(r, 15))
+		cout << "15 is in the BST" << endl;
+	else
+		cout << "15 is not in the BST" << endl;
+
+	if (TreeSearch(r, 21))
+		cout << "21 is in the BST" << endl;
+	else
+		cout << "21 is not in the BST" << endl;
 
 	system("pause");
 	return 0;
 }
 
+//Traversal function
 void inOrder(TreeNode* node)
 {
 	if (node->left != NULL)
@@ -46,28 +61,48 @@ void inOrder(TreeNode* node)
 		inOrder(node->right);
 }
 
+//insert a node into the BST
 void insert(TreeNode*& node, int num)
 {
-	if (node == NULL)
+	if (node)
 	{
-		TreeNode newNode;
-		node = &newNode;
-		node->left = NULL;
-		node->right = NULL;
+		//When num is less than the node value, recurse with left node
+		if (node->info > num)
+			insert(node->left, num);
+		//Recurse with right node
+		else
+			insert(node->right, num);
+	}
+	else //When node is null, create a new node
+	{
+		node = new TreeNode();
 		node->info = num;
 	}
-	else if (num < node->info)
-		insert(node->left, num);
-	else
-		insert(node->right, num);
 }
 
+//Return the # of nodes in the BST
 int countNodes(TreeNode* node)
 {
-	int total = 0;
+	int total = 1; //root of subtree is a node, so count it
 	if (node->left != NULL)
-		total += countNodes(node->left);
+		total += countNodes(node->left); //count root of left subtree if it exists
 	if (node->right != NULL)
-		total += countNodes(node->right);
+		total += countNodes(node->right); //coutn root of right subtree if it exists
 	return total;
+}
+
+//Return pointer to node containing num, return null if num is not in BST
+TreeNode* TreeSearch(TreeNode* node, int num)
+{
+	if (node)
+	{
+		if (node->info == num)
+			return node;					//num is equal to node value, return node pointer
+		else if (node->info > num)
+			TreeSearch(node->left, num);	//num is less than node value, check left node if it exists
+		else
+			TreeSearch(node->right, num);	//check right node if it exists
+	}
+	else
+		return node;						//return null if node is null (num not found in BST)
 }
